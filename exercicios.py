81427fcac9323449840f7a8e10cfd90ab612cece
PryscillaML <67346159+PryscillaML@users.noreply.github.com>
2024-11-14 09:29:50 -0300

    Update and rename README.md to exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+st.write(df)
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     options=["Selecione..."] + df['nomeServidor'].tolist()
+)
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+dadosFiltrados
