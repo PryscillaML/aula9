98a157f7ffe66ff5a62a9dc1b8a037bf0eb9f777
PryscillaML <67346159+PryscillaML@users.noreply.github.com>
2024-11-14 10:26:51 -0300

    Update exercico2.py

--- exercico2.py
+++ exercico2.py
@@ -6,12 +6,14 @@ df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR
 
 df.drop(columns=['Unnamed: 0'], inplace=True)
 list = ['Lat_d', 'Long_d']
-# convertendo para numeros
+
 df[list] = df[list].apply(pd.to_numeric, errors='coerce')
 estados = df['NM_UF'].unique()
 estadoFiltro = st.selectbox(
     'Qual estado selecionar?',
      estados)
+     options=["Selecione um estado"] + df['NM_UF'].tolist()
+)
 dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
